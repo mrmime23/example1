b805a8e703e7cd33258056501dd5605417109a43
mrmime23 <143339797+mrmime23@users.noreply.github.com>
2023-08-30 18:52:02 +0200

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,4 +1,4 @@
-def f1(a):
+def oneFunction(a):
     aNumber = 0
     p = None
 
@@ -14,18 +14,18 @@ def f1(a):
 
 
 #assert f1([]) == 0
-assert f1([0]) == 0
-assert f1([1]) == 1
-assert f1([1, 2, 3]) == 6
-assert f1([-1, 1]) == 0
-assert f1([1, 2, 3, 4]) == 10
-assert f1([1, 2, 3, -4]) == 0
-assert f1([-1]) == -1
-assert f1([1, 2, 3, 3]) == 6
-assert f1([3, 3, 3, 3, 3]) == 3
-assert f1([-1, -2, -3]) == -3
-assert f1([1000000]) == 1000000
-assert f1([1, 2, 3, 4, 1]) == 0
-assert f1([1, 2, 3, 4, 1, 2]) == 2
-assert f1([1, 3, 5, 42, 1])
+assert oneFunction([0]) == 0
+assert oneFunction([1]) == 1
+assert oneFunction([1, 2, 3]) == 6
+assert oneFunction([-1, 1]) == 0
+assert oneFunction([1, 2, 3, 4]) == 10
+assert oneFunction([1, 2, 3, -4]) == 0
+assert oneFunction([-1]) == -1
+assert oneFunction([1, 2, 3, 3]) == 6
+assert oneFunction([3, 3, 3, 3, 3]) == 3
+assert oneFunction([-1, -2, -3]) == -3
+assert oneFunction([1000000]) == 1000000
+assert oneFunction([1, 2, 3, 4, 1]) == 0
+assert oneFunction([1, 2, 3, 4, 1, 2]) == 2
+assert oneFunction([1, 3, 5, 42, 1])
 #assert f1([1, 2, 3, 3, 4, 1, 2, 3]) == 5
