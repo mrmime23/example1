f931304ae85b9f2acaab6220b1fd4cae0a38fe80
mrmime23 <143339797+mrmime23@users.noreply.github.com>
2023-09-01 19:02:07 +0200

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,31 +1,65 @@
-def oneFunction(a):
-    aNumber = 0
-    p = None
+def expandedFunction(b):
+    bNumber = 0
+    q = None
 
-    for v in a:
-        if p is None or v > p:
-            aNumber += v
-            p = v
-        elif v < p:
-            aNumber = 0
-            p = None
+    for x in b:
+        if q is None or x > q:
+            bNumber += x
+            q = x
+        elif x < q:
+            bNumber = 0
+            q = None
 
-    return aNumber
+    return bNumber
 
+def otherFunction(c):
+    cResult = ""
+    
+    for y in c:
+        if y % 2 == 0:
+            cResult += str(y) + " is EVEN "
+        else:
+            cResult += str(y) + " is ODD "
+    
+    return cResult
 
-#assert f1([]) == 0
-assert oneFunction([0]) == 0
-assert oneFunction([1]) == 1
-assert oneFunction([1, 2, 3]) == 6
-assert oneFunction([-1, 1]) == 0
-assert oneFunction([1, 2, 3, 4]) == 10
-assert oneFunction([1, 2, 3, -4]) == 0
-assert oneFunction([-1]) == -1
-assert oneFunction([1, 2, 3, 3]) == 6
-assert oneFunction([3, 3, 3, 3, 3]) == 3
-assert oneFunction([-1, -2, -3]) == -3
-assert oneFunction([1000000]) == 1000000
-assert oneFunction([1, 2, 3, 4, 1]) == 0
-assert oneFunction([1, 2, 3, 4, 1, 2]) == 2
-assert oneFunction([1, 3, 5, 42, 1])
-#assert f1([1, 2, 3, 3, 4, 1, 2, 3]) == 5
+def newFunction(d):
+    dSum = 0
+    for z in d:
+        dSum += z
+    return dSum
+
+def lastFunction(e):
+    eResult = ""
+    for w in e:
+        eResult += str(w) + "myName "
+    return eResult
+
+if __name__ == "__main__":
+    assert expandedFunction([]) == 0
+    assert expandedFunction([0]) == 0
+    assert expandedFunction([1]) == 1
+    assert expandedFunction([1, 2, 3]) == 6
+    assert expandedFunction([-1, 1]) == 0
+    assert expandedFunction([1, 2, 3, 4]) == 10
+    assert expandedFunction([1, 2, 3, -4]) == 0
+    assert expandedFunction([-1]) == -1
+    assert expandedFunction([1, 2, 3, 3]) == 6
+    assert expandedFunction([3, 3, 3, 3, 3]) == 3
+    assert expandedFunction([-1, -2, -3]) == -3
+    assert expandedFunction([1000000]) == 1000000
+    assert expandedFunction([1, 2, 3, 4, 1]) == 0
+    assert expandedFunction([1, 2, 3, 4, 1, 2]) == 2
+    assert expandedFunction([1, 3, 5, 42, 1]) == 0
+    
+    sample_list = [1, 2, 3, 3, 4, 1, 2, 3]
+    processed_result = otherFunction(sample_list)
+    print(processed_result)
+    
+    new_list = [10, 20, 30]
+    sum_result = newFunction(new_list)
+    print(sum_result)
+    
+    crazy_list = ["hello", "world", "python"]
+    crazy_result = crazyFunction(crazy_list)
+    print(crazy_result)
